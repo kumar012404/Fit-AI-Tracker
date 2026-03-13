1cd3462572efcc23246ef7106664612c8d55ed33
kumar012404 <vjkumar0324@gmail.com>
2026-03-13 17:52:49 +0530

    Fix font, logout button background, and cleanup CSS

--- server/server.js
+++ server/server.js
@@ -11,6 +11,10 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Routes
